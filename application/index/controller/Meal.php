017dc6312ad3086ba040d322845ba616cfd457a6
cpdelta <564732320@qq.com>
2018-02-05 19:27:51 +0800

    '2.5.2'

---
+++ application/index/controller/Meal.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\index\controller;
+use app\index\model\Menu;
+use app\index\model\Menuche;
+use think\Controller;
+use think\Request;
+use think\Db;
+
+class Meal extends Controller
+{
+    //酒店房间餐模板
+    public function meal()
+    {
+    	//$meal = Menu::all();
+    	$list = Menu::paginate(8,1000);
+    	$this->assign('list',$list);
+    	//$this->assign('meal',$meal);
+    	return $this->fetch();
+    }
+    //添加到菜单
+    public function addMeal(Request $request)
+    {
+    	$mid = $request->param('mid');
+    	$uid = Session('uid');
+    	$data['mid'] = $mid;
+    	$data['uid'] = $uid;
+        $money = Db::name('menu')->where('mid',"$mid")->find();
+        $data['m_money'] = $money['m_money'];
+    	$result = Menuche::create($data);
+    	if(empty($result)){
+    		echo 1;
+    	}
+    } 
+}
---
+++ application/index/model/Menu.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\index\Model;
+
+use think\Model;
+
+class Menu extends Model
+{
+
+}
